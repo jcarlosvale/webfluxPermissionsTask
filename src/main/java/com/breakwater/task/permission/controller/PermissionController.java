e43915a2f28127f1f4daecd6739668d29c315109
SOUS021 <joao.vale@arvato.com>
2020-10-24 09:56:57 +0300

    initial commit

---
+++ src/main/java/com/breakwater/task/permission/controller/PermissionController.java
@@ -0,0 +1,10 @@
+package com.breakwater.task.permission.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PermissionController {
+
+    // TODO: Implement me
+
+}
